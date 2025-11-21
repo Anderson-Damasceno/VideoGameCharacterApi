6905d1cd46e52ffdd5c392cec5a4a45856f30a66
Anderson Damasceno <andersondamascenodev@gmail.com>
2025-11-20 21:01:17 -0300

    First half hour

---
+++ VideoGameCharacterApi/Controllers/VideoGameCharacters.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameCharacterApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VideoGameCharacters : ControllerBase
+{
+}
---
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -0,0 +1,31 @@
+﻿using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Services;
+
+public class VideoGameCharacterService : IVideoGameCharacterService
+{
+    public Task<Character> AddCharacterAsync(Character character)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteCharacterAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Character>> GetAllCharactersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Character?> GetCharacterByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateCharacterAsync(int id, Character character)
+    {
+        throw new NotImplementedException();
+    }
+}
